02007fc26d03f0b48e284e795ff0cdc7bc9eaf81
David Hernández puerta <David Hernndez puerta>
2016-12-30 19:28:08 +0100

    Anadido tabla dinamica
    Anadido tabla dinamica, y estilos de dicha tabla y actualizado de
    index.html

--- cruz_raya/js/cruz_raya.js
+++ cruz_raya/js/cruz_raya.js
@@ -1,4 +1,21 @@
 function my_function() {
 
     console.log("Hello Cruz en raya");
+    var htmlarray_id = document.getElementById("my_table");
+
+    var result = '';
+
+
+    for (var i = 0; i < 3; i++) {
+
+        result += '<tr>';
+
+        for (var j = 0; j < 3; j++) {
+            result += '<td>' + 'x' + "</td>";
+        }
+        result += "</tr>";
+    }
+
+    htmlarray_id.innerHTML = result;
+    
 }
